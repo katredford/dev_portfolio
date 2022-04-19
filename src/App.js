41590831919d4b007cf572cda34527dc6c93cbda
katredford <katharine.redford@gmail.com>
2022-04-19 18:45:03 -0500

    trying to get this hero image to display

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ import "./assets/index.css";
 import "./assets/responsive.css";
 import "./assets/images/hero.png";
 import Nav from "./components/Nav";
-import hero from "./assets/images/hero.png";
 
 function App() {
   const [currentPageState, setCurrentPage] = useState("about");
   return (
     <div>
       <Nav setCurrentPage={setCurrentPage}></Nav>
-      <section>
-        <div className="hero hero-pic">
-          <img src={hero} alt="cover" />
-
-          <h2>Where the magic happens!</h2>
-        </div>
+      <section className="hero ">
+        <h2>Where the magic happens!</h2>
       </section>
       <main>
         <CurrentPage currentPageState={currentPageState} />
