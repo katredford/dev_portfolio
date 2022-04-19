8f890f4bef2b87805372c93e2f40b10746bd2f5f
katredford <katharine.redford@gmail.com>
2022-04-19 12:55:41 -0500

    Updates

---
+++ precache-manifest.51c1ec5c4644aa8b47cdac0beb472700.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e3d9646d446671ff9b3",
+    "url": "/dev_portfolio/static/css/main.64db34be.chunk.css"
+  },
+  {
+    "revision": "1e3d9646d446671ff9b3",
+    "url": "/dev_portfolio/static/js/main.1e3d9646.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/dev_portfolio/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "1598ae4342e7d4517083",
+    "url": "/dev_portfolio/static/js/2.1598ae43.chunk.js"
+  },
+  {
+    "revision": "382910735f0e00701c03",
+    "url": "/dev_portfolio/static/js/runtime~main.38291073.js"
+  },
+  {
+    "revision": "f4f80c71077a29c64987df032a5d081c",
+    "url": "/dev_portfolio/static/media/gardenThyme1.f4f80c71.png"
+  },
+  {
+    "revision": "6ac05355203629835a3808306a924210",
+    "url": "/dev_portfolio/static/media/roastme.home.6ac05355.png"
+  },
+  {
+    "revision": "e586fec3cd84bbe9b5cc8f5cbc7dd63a",
+    "url": "/dev_portfolio/static/media/bbcm.home.e586fec3.png"
+  },
+  {
+    "revision": "24562200bb76b2afe8aef1c2439f0eb5",
+    "url": "/dev_portfolio/static/media/kittenaround.home.24562200.png"
+  },
+  {
+    "revision": "9e5d5c632cd0bd584a9c16a50e1ad6a2",
+    "url": "/dev_portfolio/static/media/hero.9e5d5c63.png"
+  },
+  {
+    "revision": "705dc41167279fc0c80f86d47e674acb",
+    "url": "/dev_portfolio/static/media/katie.705dc411.JPG"
+  },
+  {
+    "revision": "49dd63bb9cb60b2aecf10e17b66a14d0",
+    "url": "/dev_portfolio/index.html"
+  }
+];
