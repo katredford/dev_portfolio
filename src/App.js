26e389dfe9a62086e8ea63703c5c0444f0a3f5d1
katredford <katharine.redford@gmail.com>
2022-04-19 18:35:31 -0500

    add react-webpack

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import "./assets/index.css";
 import "./assets/responsive.css";
 import "./assets/images/hero.png";
 import Nav from "./components/Nav";
-// import hero from "./assets/images/hero.png";
+import hero from "./assets/images/hero.png";
 
 function App() {
   const [currentPageState, setCurrentPage] = useState("about");
   return (
     <div>
       <Nav setCurrentPage={setCurrentPage}></Nav>
-      <section className="hero hero-pic">
-        {/* <img src={hero} alt="cover" /> */}
-        <h2>Where the magic happens!</h2>
+      <section>
+        <div className="hero hero-pic">
+          <img src={hero} alt="cover" />
+
+          <h2>Where the magic happens!</h2>
+        </div>
       </section>
       <main>
         <CurrentPage currentPageState={currentPageState} />
