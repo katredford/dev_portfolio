43edeaf98aee8f03fd885e391d5da7e8ad4bd1d6
katredford <katharine.redford@gmail.com>
2022-04-19 19:26:33 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.05fed67541ecf9e4ac9adec6348134cb.js"
+  "./precache-manifest.b644b39aca6ce996daaf095e631bc576.js"
 );
 
 workbox.clientsClaim();
