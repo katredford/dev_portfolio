9a391683d3d5853473bb3fc5e30a28d6f2605c59
katredford <katharine.redford@gmail.com>
2022-04-19 19:21:29 -0500

    add header component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import "./assets/index.css";
 import "./assets/responsive.css";
 import "./assets/images/hero.png";
 import Nav from "./components/Nav";
+import Header from "./components/Header";
 
 function App() {
   const [currentPageState, setCurrentPage] = useState("about");
   return (
     <div>
       <Nav setCurrentPage={setCurrentPage}></Nav>
-      <section className="hero ">
-        <h2>Where the magic happens!</h2>
-      </section>
+      <Header />
       <main>
         <CurrentPage currentPageState={currentPageState} />
       </main>
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../../assets/index.css";
+import "../../assets/responsive.css";
+import "../../assets/images/hero.png";
+
+function Header() {
+  return (
+    <section className="hero">
+      <h2>Where the magic happens!</h2>
+    </section>
+  );
+}
+
+export default Header;
